116f05ad848497d6863a0c6884955fdaafff35d4
hansikasundaresan <55006307+hansikasundaresan@users.noreply.github.com>
2020-05-29 21:16:04 -0700

    Add files via upload

--- websitejs.js
+++ websitejs.js
@@ -4,8 +4,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.padding = "40px 0px 20px 10px";
+    document.getElementById("header").style.padding = "20px 0px 20px 0px";
   } else {
-    document.getElementById("header").style.padding = "50px 0px 50px 10px";
+    document.getElementById("header").style.padding = "40px 0px 40px 0px";
   }
 }
+
