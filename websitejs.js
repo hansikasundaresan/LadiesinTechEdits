35add4d8c597fae47b516aa14037b4bcbca6c37a
hansikasundaresan <55006307+hansikasundaresan@users.noreply.github.com>
2020-06-02 13:42:22 -0700

    Add files via upload

--- websitejs.js
+++ websitejs.js
@@ -10,3 +10,6 @@ function scrollFunction() {
   }
 }
 
+var shiftWindow = function() { scrollBy(0, -100) };
+window.addEventListener("hashchange", shiftWindow);
+function load() { if (window.location.hash) shiftWindow(); }
