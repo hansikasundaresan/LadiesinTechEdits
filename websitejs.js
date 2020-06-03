a2169b501cfa490636f135d35fb15fb15f75b142
hansikasundaresan <55006307+hansikasundaresan@users.noreply.github.com>
2020-06-02 17:37:24 -0700

    Add files via upload

--- websitejs.js
+++ websitejs.js
@@ -1,3 +1,7 @@
+window.onscroll = function() {scrollFunction()};
+window.onresize = function(){ location.reload(); }
+
+
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header").style.padding = "20px 0px 20px 0px";
@@ -5,18 +9,15 @@ function scrollFunction() {
     document.getElementById("header").style.padding = "40px 0px 40px 0px";
   }
 }
-if (matchMedia) {
-const mq = window.matchMedia("(max-width: 767px)");
-mq.addListener(WidthChange);
-WidthChange(mq);
-}
 
-function WidthChange(mq) {
+const mq = window.matchMedia( "(max-width: 767px)" );
 if (mq.matches) {
-var shiftWindow = function() { scrollBy(0, -200) };
+var shiftWindow = function() { scrollBy(0, -180) };
 window.addEventListener("hashchange", shiftWindow);
-function load() { if (window.location.hash) shiftWindow(); }} else {
-var shiftWindow = function() { scrollBy(0, -120) };
+function load() { if (window.location.hash) shiftWindow(); }
+} else {
+var shiftWindow = function() { scrollBy(0, -100) };
 window.addEventListener("hashchange", shiftWindow);
-function load() { if (window.location.hash) shiftWindow(); }}
+function load() { if (window.location.hash) shiftWindow(); }
+    
 }
